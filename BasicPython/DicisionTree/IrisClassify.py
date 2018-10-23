0c47cf6f7c4479ea4014a624f982425a401ac38d
Shea6031 <xj11306031@126.com>
2018-10-23 21:41:24 +0800

    iris classify by decision tree

--- BasicPython/DicisionTree/IrisClassify.py
+++ BasicPython/DicisionTree/IrisClassify.py
@@ -1,5 +1,24 @@
 from sklearn.datasets import load_iris
+from sklearn.cross_validation import train_test_split
 
+from sklearn.tree import DecisionTreeClassifier
 # load iris
 iris = load_iris()
-print(iris.target_names)
+
+iris_feature = iris.data
+iris_label = iris.target
+train_x, test_x, train_y, test_y= train_test_split(iris_feature, iris_label)
+
+decTree = DecisionTreeClassifier(max_depth=2)
+decTree.fit(train_x,train_y)
+predict = decTree.predict(test_x)
+
+from sklearn.tree import export_graphviz
+export_graphviz(
+decTree,
+out_file="iris_tree.dot",
+feature_names=iris.feature_names,
+class_names=iris.target_names,
+rounded=True,
+filled=True
+)
